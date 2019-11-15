91e2689a1d05208120b269934cd57ca8d5dd39d8
조승현 <cleanjjsh@naver.com>
2019-11-15 14:54:23 +0900

    dle send code

--- c++++/socket/unity_socekt/Socket/Assets/Socket.cs
+++ c++++/socket/unity_socekt/Socket/Assets/Socket.cs
@@ -9,19 +9,10 @@ using UnityEngine;
 
 public class Socket : MonoBehaviour
 {
-    #region private members 	
-    /// <summary> 	
-    /// TCPListener to listen for incomming TCP connection 	
-    /// requests. 	
-    /// </summary> 	
-    private TcpListener tcpListener;
-    /// <summary> 
-    /// Background thread for TcpServer workload. 	
-    /// </summary> 	
-    private Thread tcpListenerThread;
-    /// <summary> 	
-    /// Create handle to connected tcp client. 	
-    /// </summary> 	
+    #region private members 
+    // TCPListener to listen for incomming TCP connection 	requests. 	
+    private TcpListener tcpListener; /// Background thread for TcpServer workload. 	
+    private Thread tcpListenerThread; /// Create handle to connected tcp client. 	
     private TcpClient connectedTcpClient;
     #endregion
 
@@ -43,9 +34,8 @@ public class Socket : MonoBehaviour
         }
     }
 
-    /// <summary> 	
-    /// Runs in background TcpServerThread; Handles incomming TcpClient requests 	
-    /// </summary> 	
+    /// Runs in background TcpServerThread; Handles incomming TcpClient requests
+ 
     private void ListenForIncommingRequests()
     {
         try
@@ -79,35 +69,6 @@ public class Socket : MonoBehaviour
         catch (SocketException socketException)
         {
             Debug.Log("SocketException " + socketException.ToString());
-        }
-    }
-    /// <summary> 	
-    /// Send message to client using socket connection. 	
-    /// </summary> 	
-    private void SendMessage()
-    {
-        if (connectedTcpClient == null)
-        {
-            return;
-        }
-
-        try
-        {
-            // Get a stream object for writing. 			
-            NetworkStream stream = connectedTcpClient.GetStream();
-            if (stream.CanWrite)
-            {
-                string serverMessage = "This is a message from your server.";
-                // Convert string message to byte array.                 
-                byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
-                // Write byte array to socketConnection stream.               
-                stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
-                Debug.Log("Server sent his message - should be received by client");
-            }
-        }
-        catch (SocketException socketException)
-        {
-            Debug.Log("Socket exception: " + socketException);
         }
     }
 }
